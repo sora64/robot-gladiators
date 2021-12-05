94a13a390db81f82be9d068855b146e7feda39af
Alan Phelps <Percival@Percival.local>
2021-12-05 15:04:22 -0500

    added win or lose messages if health reaches 0

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,7 +2,7 @@ var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 
-//You can also log multiple values at once like this 
+// You can also log multiple values at once like this 
 console.log(playerName, playerAttack, playerHealth);
 
 var enemyName = "Roborto";
@@ -14,7 +14,7 @@ console.log(enemyName, enemyHealth, enemyAttack);
 // create function
 var fight = function() {
     // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators")
+    window.alert("The fight has begun!")
 
     //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable.
     enemyHealth = enemyHealth - playerAttack;
@@ -23,6 +23,15 @@ var fight = function() {
     console.log(
         playerName + " attacked " +enemyName+". "+enemyName+" now has "+enemyHealth+" health remaining."
     );
+
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName+ " has died!");
+    }
+    else {
+        window.alert(enemyName+ " still has "+enemyHealth+" health left.");
+    }
+
     // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
     playerHealth = playerHealth - enemyAttack;
 
@@ -30,6 +39,14 @@ var fight = function() {
     console.log(
         enemyName +" attacked "+playerName+". "+playerName+" now has "+playerHealth+" health remaining."
     );
-};
+
+    // check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName+ " has died!");
+    }
+    else {
+        window.alert(playerName+ " still has "+playerHealth+" health left.");
+    }
+}
 // execute function
 fight();
