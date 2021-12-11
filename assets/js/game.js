f0f55202654417d769c7ecaf20ce7c3d15e35c1e
Alan Phelps <phelpsa64@gmail.com>
2021-12-10 20:05:56 -0500

    fixed typo

--- assets/js/game.js
+++ assets/js/game.js
@@ -195,7 +195,7 @@ var randomNumber = function(min, max) {
 var getPlayerName = function() {
     var name = "";
     while (name === "" || name === null) {
-        name = prompt("what is your robot's name?");
+        name = prompt("What is your robot's name?");
     };
     console.log("Your robot's name is " +name);
     return name;
