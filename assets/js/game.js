6cc794a9651c27d3d98ad2c9397cc3b8e04e420e
Alan Phelps <phelpsa64@gmail.com>
2021-12-11 01:11:56 -0500

    added high score functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -151,17 +151,26 @@ var startGame = function() {
 
 // function to end the entire game
 var endGame = function() {
-    // if player is still alive, player wins!
-    if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You have a score of "+playerInfo.money+".");
+    window.alert("The game has now ended. Let's see how you did!");
+
+    // check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    highScore = highScore || 0;
+
+    // if player has more money than high score, player has new high score
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
     } else {
-        window.alert("You've lost your robot in battle! Game Over!");
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
+
     // ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
-    if(playAgainConfirm) {
-        // restart the game
+    if (playAgainConfirm) {
         startGame();
     } else {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
